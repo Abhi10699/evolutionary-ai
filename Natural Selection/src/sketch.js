f0b6a781f1a44ea7dd28034f7305fc773e5be409
Abhi Patel <patelabhi10699@gmail.com>
2023-05-17 13:49:22 -0400

    used the world class to run the simulation

--- Natural Selection/src/sketch.js
+++ Natural Selection/src/sketch.js
@@ -1,34 +1,11 @@
-let agents = new Array(200);
-let food = new Array(50);
-
+let world = new World();
 function setup() {
   createCanvas(1280, 720);
-  
-  // generate population
-  for (let i = 0; i < agents.length; i++) {
-    const posX = 10
-    const posY = i*14 + 10;
-    agents[i] = new Agent(posX, posY);
-  }
-
-  // generate food
-
-  for(let i=0;i<food.length;i++) {
-    const posX = random(width/2  - 100, width/2 + 400);
-    const posY = random(height/2 - 200,height/2 + 200);
-
-    food[i] = new Food(posX, posY);
-  }
-
+  world.init(width, height);
 }
 
 function draw() {
   background(51);
-  food.forEach(food => food.draw())
-  agents.forEach(agent => {
-    agent.move(Math.floor(random(0,5)))
-    agent.draw()
-  })
-
-  noLoop()
+  world.evolve();
+  // noLoop();
 }
