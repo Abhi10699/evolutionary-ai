0f5cce584ee94ddfe18f837fab652a5a70676d58
Abhi Patel <patelabhi10699@gmail.com>
2023-05-17 13:48:06 -0400

    implemented a basic neural network that takes actions

--- Natural Selection/lib/brain.js
+++ Natural Selection/lib/brain.js
@@ -1,3 +1,28 @@
 class Brain {
-  
+  brain = null;
+
+
+  /**
+   * Input -> [posX, posY, foodPosX, foodPosY]
+   * Output -> action probablities
+   * 
+   */
+
+  constructor() {
+    this.brain = tf.sequential();
+    this.brain.add(
+      tf.layers.dense({ units: 4, inputShape: [4], activation: "softmax" })
+    );
+  }
+
+  mutateBrain() {}
+
+  chooseAction(inputs) {
+    return tf.tidy(async () => {
+      const inputTensor = tf.tensor2d([inputs]);
+      const actionProbablities = await this.brain.predict(inputTensor).data()
+      const maxActionProbablity = await tf.argMax(actionProbablities).data();
+      return maxActionProbablity[0];
+    });
+  }
 }
