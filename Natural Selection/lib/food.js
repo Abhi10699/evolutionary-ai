be005fc77466bf5825cd04696477bbc107dbfe11
Abhi Patel <patelabhi10699@gmail.com>
2023-05-18 15:59:52 -0400

    food will despawn once eaten

--- Natural Selection/lib/food.js
+++ Natural Selection/lib/food.js
@@ -1,19 +1,23 @@
 class Food {
   position;
   isPoison = false;
+  isEaten = false;
 
   constructor(posX, posY) {
     this.position = createVector(posX, posY);
-    if(random() < 0.3) {
-      this.isPoison = true;
-    }
+    // if(random() < 0.3) {
+    //   this.isPoison = true;
+    // }
   }
-
 
   draw() {
     push();
     fill(this.isPoison ? "red" : "green");
-    circle(this.position.x, this.position.y,10,10)
+    circle(this.position.x, this.position.y, 10, 10);
     pop();
+  }
+
+  eat() {
+    this.isEaten = true;
   }
 }
